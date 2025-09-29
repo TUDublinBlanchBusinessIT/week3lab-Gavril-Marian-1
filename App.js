716b7f9236909fec885f3f0868544fac6041c8d4
B00165945 Gavril Marian <B00165945@mytudublin.ie>
2025-09-29 16:41:27 +0100

    Step 2

--- App.js
+++ App.js
@@ -2,16 +2,35 @@ import {View, Text, TextInput} from 'react-native';
 import React, {useState} from 'react';
 import Logo from './components/Logo';
 
-
 export default function App(){
   const [fullname, setFullname] = useState("Eoin");
+  const [fname, setFname] = useState("Joe");
+  const [lname, setLname] = useState("Bloggs");
+  const [dob, setDob] = useState("22 August 1990");
   return(
     <View> 
+    <Logo />
       <Text>Hello, World {fullname}</Text>
-      <TextInput placeholder="enter your name" onChangeText={(value)=>setFullname(value)}></TextInput>
+   
+      <TextInput
+        placeholder="Enter your name"
+        onChangeText={setFullname}
+      />
+      <TextInput
+        placeholder="Enter your firstname"
+        onChangeText={setFname}
+      />
+      <TextInput
+        placeholder="Enter your lastname"
+        onChangeText={setLname}
+      />
+      <TextInput
+        placeholder="Enter your date of birth"
+        onChangeText={setDob}
+      />
+
+      <Text>Hello {fname} {lname}. You were born on {dob}</Text>
     </View>
-  )
-};
-
-
-
+  );
+}
+  
